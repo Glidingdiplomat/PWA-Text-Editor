987d74eb5880caaff6dedf925d7382fa217a262d
Glidingdiplomat <yankeefan1246@gmail.com>
2023-11-28 15:53:55 -0500

    updated commit.
    reference and grabbed code from week 19 PWA mini project, ask chatgpt few questions in relation and typed more read me info.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # PWA-Text-Editor
- to create notes or snippets of code without a code editor.
+
+to create notes or snippets of code without a code editor.
+
+# User Story
+
+As a developer i want to create notes or code snippets with or without an internet connection so that i can reliably retrieve them for later use.
+
+# Resources
+
+chatgpt -ask few questions related to the todos.
+Class - Lectures from week 19 mini project images below a side by side on what i used left is from mini project right is the challenge.
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 const { InjectManifest } = require("workbox-webpack-plugin");
+const { title } = require("process");
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -17,10 +18,47 @@ module.exports = () => {
       filename: "[name].bundle.js",
       path: path.resolve(__dirname, "dist"),
     },
-    plugins: [],
-
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+      }),
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src.js",
+      }),
+      new WebpackPwaManifest({
+        name: "Just Another Text Editor",
+        short_name: "JATE",
+        description: "PWA Text Editor App",
+        background_color: "#ffffff",
+        start_url: "./",
+        publicPath: "./",
+        icons: [
+          {
+            src: path.resolve("./src/images/logo.png"),
+            sizes: [16, 36, 128, 192, 256, 384, 532],
+            destination: path.json("assets", "icons"),
+          },
+        ],
+      }),
+    ],
     module: {
-      rules: [],
+      rules: [
+        {
+          test: /\/css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/present-env"],
+            },
+          },
+        },
+      ],
     },
   };
 };
